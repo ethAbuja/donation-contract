c804f0337679af9f8d3b5419a7d54d866cdbcadb
casweeney <casweeno2000@gmail.com>
2023-12-05 22:23:27 +0100

    WIP: smart contract optimization

--- src/Donation.sol
+++ src/Donation.sol
@@ -6,6 +6,7 @@ import "./interface/IERC20.sol";
 contract Donation {
     event Donated(address indexed donor, uint256 amount);
     event Withdrawn(address indexed receiver, uint256 amount);
+    event OwnerChanged(address newOwner);
 
     address public owner;
     mapping(address => uint256) donations;
@@ -21,13 +22,29 @@ contract Donation {
 
         IERC20(_stableToken).transferFrom(msg.sender, address(this), _amount);
 
-        donations[msg.sender] += _amount;
+        donations[msg.sender] = donations[msg.sender] + _amount;
 
         emit Donated(msg.sender, _amount);
     }
 
     function withdraw(address _stableToken) external {
         onlyOwner();
+
+        uint256 contractBalance = IERC20(_stableToken).balanceOf(address(this));
+
+        require(contractBalance > 0, "no funds in contract");
+
+        IERC20(_stableToken).transfer(owner, contractBalance);
+
+        emit Withdrawn(owner, contractBalance);
+    }
+
+    function changeOwner(address _newOwner) external {
+        onlyOwner();
+
+        owner = _newOwner;
+
+        emit OwnerChanged(_newOwner);
     }
 
     function showContractBalance(address _stableToken) external view returns (uint) {
@@ -37,4 +54,6 @@ contract Donation {
     function onlyOwner() private {
         require(msg.sender == owner, "not owner");
     }
+
+    receive() external payable {}
 }
