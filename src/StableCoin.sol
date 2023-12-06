5d657414664a8233b9fc083060f5ad5e2132fc55
casweeney <casweeno2000@gmail.com>
2023-12-06 10:27:54 +0100

    WIP: created a stable coin

---
+++ src/StableCoin.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract StableCoin is ERC20("Stable Coin", "STB") {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+        _mint(msg.sender, 100000e18);
+    }
+
+    function mint(uint _amount) external {
+        require(msg.sender == owner, "you are not owner");
+        _mint(msg.sender, _amount * 1e18);
+    }
+}
